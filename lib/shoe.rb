acfe560786ff8f75f79d4776d0b2fddcd2e80cea
Kari O'Neal <triadfirewood@gmail.com>
2018-08-10 21:18:32 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,6 @@ class Shoe
     
   def initialize(brand)
     @brand = brand
-  end
-
-  def brand= (brand)
-    @brand = brand
     BRANDS << brand
   end
  
@@ -18,6 +14,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  
-    
 end
